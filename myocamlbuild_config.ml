af41f1571d180043e8afbd6b8425be2f64801505
ygrek <ygrek@autistici.org>
2014-09-24 14:23:56 +0800

    use 4.02 builtin ppx tag

--- myocamlbuild_config.ml
+++ myocamlbuild_config.ml
@@ -1,6 +1,4 @@
 open Ocamlbuild_plugin;;
 
-mark_tag_used "use_menhir";;
-flag ["ocaml";"compile";"ppx"] (S [A"-ppx";A"./ppx/ppx_inject.byte"]);;
 flag ["ocaml";"compile";"dsource"] (S [A"-dsource"]);;
-dep ["ppx"] ["ppx/ppx_inject.byte"];;
+dep ["file:src/gdbmi_proto.ml"] ["ppx/ppx_inject.byte"];;
