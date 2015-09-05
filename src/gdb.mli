74857b458bc87d3387208ae23166931b0779c4cb
ygrek <ygrek@autistici.org>
2015-09-05 01:11:33 -0700

    add doc comments

--- src/gdb.mli
+++ src/gdb.mli
@@ -4,16 +4,29 @@ module Proto = Gdbmi_proto
 
 exception Parse_error of string * string * string
 
+(** type representing GDB/MI session *)
 type gdb
 (*
 val send_command : gdb -> string -> unit Lwt.t
 val read_input : gdb -> Types.output_record list Lwt.t
 *)
+
+(** launch GDB *)
 val launch : ?dump:string -> unit -> gdb Lwt.t
+
+(** terminate GDB session and process *)
 val quit : gdb -> unit Lwt.t
 
+(** @retrun GDB process *)
 val inferior : gdb -> Lwt_process.process
+
+(** execute GDB/MI command - low-level interface *)
 val execute : gdb -> string -> Types.output_record list Lwt.t
+
+(** exevcute GDB/MI, fail on error, ignore result *)
+val run : gdb -> ('a, unit, string, unit Lwt.t) format4 -> 'a
+
+(** execute GDB/MI command and get the result *)
 val mi : gdb -> string -> string list -> Types.result Lwt.t
 
 val parse_io : string -> Types.input_output
@@ -30,6 +43,8 @@ sig
   val break_commands : gdb -> int -> string list -> unit Lwt.t
 end
 
-val run : gdb -> ('a, unit, string, unit Lwt.t) format4 -> 'a
+(** @return demangled human readable function name *)
 val demangle : string -> string
+
+(** pretty-print frame information *)
 val show_frame_function : Proto.frame -> string
