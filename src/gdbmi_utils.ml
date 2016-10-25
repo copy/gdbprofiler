5a0c44d303c2b5a9bb57997d697ec4f0c6c0a2fb
Fabian <copy@copy.sh>
2016-10-25 23:25:58 +0200

    Better error messages

--- src/gdbmi_utils.ml
+++ src/gdbmi_utils.ml
@@ -3,10 +3,13 @@ open Gdbmi_types
 
 type 'a values = 'a list
 
-let string = function String x -> x | _ -> assert false
-let int = function String x -> int_of_string x | _ -> assert false
-let named name (name',x) = if name = name' then x else assert false
-let tuple = function Tuple x -> x | _ -> assert false
-let list f = function List x -> List.map f x | _ -> assert false
-let values f = function Values x -> List.map f x | _ -> assert false
+let failwithf fmt = Printf.ksprintf (fun s -> failwith s) fmt
+
+let string = function String x -> x | v -> failwithf "expected String, got %s" @@ show_value v
+let int = function String x -> int_of_string x | v -> failwith "expected int, got %s" @@ show_value v
+let named name (name',x) = if name = name' then x else failwithf "expected name '%s' got '%s'" name name'
+let tuple = function Tuple x -> x | v -> failwithf "expected tuple, got '%s'" @@ show_value v
+let list f = function List x -> List.map f x | v -> failwithf "expected list, got '%s'" @@ show_value v
+(* let list f = function List x -> List.map f x | Values x -> List.map f x | v -> failwithf "expected list, got '%s'" @@ show_value v *)
+let values f = function Values x -> List.map f x | v -> failwithf "expected values, got '%s'" @@ show_value v
 let assoc_map map k l = match try Some (List.assoc k l) with Not_found -> None with None -> None | Some x -> Some (map x)
