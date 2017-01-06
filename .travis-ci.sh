cdcd24c288fac1a2b9c1198693ba1df73fdae6b3
Fabian <copy@copy.sh>
2017-01-06 17:33:43 -0500

    Fix travis (2)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,9 +6,8 @@ eval `opam config env`
 opam pin add --yes -n $(pwd)
 opam install --yes gdb
 
-ls -lah
-ls -lah $(pwd)
-$(pwd)/run_integration_test.native
+make
+./run_integration_test.native
 
 #opam install --yes menhir extlib lwt ppx_deriving ppx_deriving_yojson oasis containers
 #make
--- myocamlbuild_config.ml
+++
@@ -1,5 +0,0 @@
-open Ocamlbuild_plugin
-
-let () =
-  flag ["ocaml";"pp";"pp_cppo"] & S[A"cppo"; A"-V"; A (Printf.sprintf "OCAML:%s" Sys.ocaml_version)];
-  ()
