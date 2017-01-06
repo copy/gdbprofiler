5abd0639c089084fdcb255e646e20d640fd27fb1
Fabian <copy@copy.sh>
2017-01-06 17:23:37 -0500

    Travis fix

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,7 +6,9 @@ eval `opam config env`
 opam pin add --yes -n $(pwd)
 opam install --yes gdb
 
-./run_integration_test.native
+ls -lah
+ls -lah $(pwd)
+$(pwd)/run_integration_test.native
 
 #opam install --yes menhir extlib lwt ppx_deriving ppx_deriving_yojson oasis containers
 #make
