a4098ff01fbf9c6ba1ba926a58d2846ebe2c2cd0
Fabian <copy@copy.sh>
2017-01-16 15:42:57 -0500

    Can't run integration test on travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,9 @@
 wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
 sh .travis-ocaml.sh
 
-sudo sysctl 'kernel.yama.ptrace_scope=0'
-
 eval `opam config env`
 
 opam pin add --yes -n $(pwd)
 opam install --yes gdb
 
 make
-RMP_LOG_VERBOSE=1 ./run_integration_test.native
-
-#opam install --yes menhir extlib lwt ppx_deriving ppx_deriving_yojson oasis containers
-#make
