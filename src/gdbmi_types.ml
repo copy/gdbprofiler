73f504f18860824294db70107ae9ce4121428cc8
Fabian <copy@copy.sh>
2017-08-31 21:02:21 +0200

    Fix compilation on 4.02.3

--- src/gdbmi_types.ml
+++ src/gdbmi_types.ml
@@ -5,32 +5,57 @@ and value =
 | Tuple of vars
 | List of vars
 | Values of value list
-[@@deriving show]
+let rec show_vars v = CCFormat.(to_string (list (pair string_quoted (of_to_string show_value))) v)
+and show_value = function
+  | String s -> "String(" ^ CCFormat.(to_string string_quoted s) ^ ")"
+  | _ -> ""
 
 type stream_type = Console | Log | Target
-[@@deriving show]
+let show_stream_type = function
+  | Console -> "Console"
+  | Log -> "Log"
+  | Target -> "Target"
 
 type async_type = Exec | Status | Notify
-[@@deriving show]
+let show_async_type = function
+  | Exec -> "Exec"
+  | Status -> "Status"
+  | Notify -> "Notify"
 
 type gtoken = string option
-[@@deriving show]
+let show_gtoken g = CCFormat.(to_string (opt string_quoted) g)
 
 type result =
 | Done of vars
 | Connected
 | OpError of string * string option
 | Exit
-[@@deriving show]
+let show_result = function
+  | Done v -> "Done(" ^ show_vars v ^ ")"
+  | Connected -> "Connected"
+  | OpError (s1, s2) ->
+    "OpError(" ^
+    CCFormat.(to_string string_quoted s1) ^ ", " ^
+    CCFormat.(to_string (opt string_quoted) s2) ^ ")"
+  | Exit -> "Exit"
 
 type output_record =
 | Stream of stream_type * string
 | Async of gtoken * async_type * string * vars
 | Result of gtoken * result
-[@@deriving show]
+let show_output_record = function
+  | Stream (stream_type, s) ->
+    "Stream(" ^
+    show_stream_type stream_type ^ ", " ^
+    CCFormat.(to_string string_quoted s) ^ ")"
+  | Async (token, async_type, s, vars) ->
+    "Async(" ^
+    show_gtoken token ^ ", " ^
+    show_async_type async_type ^ ", " ^
+    CCFormat.(to_string string_quoted s) ^ ", " ^
+    show_vars vars ^ ")"
+  | Result (token, result) -> "Result(" ^ show_gtoken token ^ ", " ^ show_result result ^ ")"
 
 type input_type = MI of gtoken | CLI
-[@@deriving show]
 
 type input_output = Prompt | Input of input_type | Output of output_record
-[@@deriving show]
