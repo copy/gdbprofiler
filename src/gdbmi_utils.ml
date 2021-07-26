6636af09dafff4fea743271be7db20c56c396957
Fabian <copy@copy.sh>
2021-07-26 18:40:36 -0500

    minor fix

--- src/gdbmi_utils.ml
+++ src/gdbmi_utils.ml
@@ -6,7 +6,7 @@ type 'a values = 'a list
 let failwithf fmt = Printf.ksprintf (fun s -> failwith s) fmt
 
 let string = function String x -> x | v -> failwithf "expected String, got %s" @@ show_value v
-let int = function String x -> int_of_string x | v -> failwith "expected int, got %s" @@ show_value v
+let int = function String x -> int_of_string x | v -> failwithf "expected int, got %s" @@ show_value v
 let named name (name',x) = if name = name' then x else failwithf "expected name '%s' got '%s'" name name'
 let tuple = function Tuple x -> x | v -> failwithf "expected tuple, got '%s'" @@ show_value v
 let list f = function List x -> List.map f x | v -> failwithf "expected list, got '%s'" @@ show_value v
