8e1f5e3220bbe0966373a63c655bfc42a5871946
Fabian <copy@copy.sh>
2021-07-26 19:21:56 -0500

    github ci

--- .travis-ci.sh
+++
@@ -1,9 +0,0 @@
-wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
-sh .travis-ocaml.sh
-
-eval `opam config env`
-
-opam pin add $(pwd) --yes --no-action
-opam install gdbprofiler --yes --deps-only
-
-make unit-test
